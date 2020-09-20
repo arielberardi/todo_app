229021753531cae7789bd72ec325a7b9b8cc7ff1
Ariel Berardi <aberardi@rlink.com.ar>
2020-09-20 15:59:04 -0300

    Updates

---
+++ precache-manifest.ffeb17437d8f2c07f428770ace88eb56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f912e09904eaf9b27c6032d2fd2e1c",
+    "url": "/todo_app/index.html"
+  },
+  {
+    "revision": "add99d519d42eb1e4e1a",
+    "url": "/todo_app/static/css/main.63c8257b.chunk.css"
+  },
+  {
+    "revision": "c7a9ef31eddf6a3ad594",
+    "url": "/todo_app/static/js/2.fdda74d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_app/static/js/2.fdda74d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "add99d519d42eb1e4e1a",
+    "url": "/todo_app/static/js/main.f72e1ec4.chunk.js"
+  },
+  {
+    "revision": "fd0554993c1b85667a36",
+    "url": "/todo_app/static/js/runtime-main.24c42b9d.js"
+  }
+]);
